d938df1aa694febb6dbab1f23978806bfd87214c
SudoCerb <naude.michael.h@gmail.com>
2019-04-14 22:40:25 +0200

    First commit, added REST API to access items, stores, and user authentication.

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+
+from security import authenticate, identity as identity_function
+from resources.user import UserRegister
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
+
+app = Flask(__name__)
+# this turns off the FLASK SQLAlchemy Trackcer to save resources
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db' # doesn't have to be sqlite
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
+app.secret_key = 'gerald'
+api = Api(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+# use this line of code to change the auth endpoint URL
+#app.config['JWT_AUTH_URL_RULE'] = '/login'
+jwt = JWT(app, authenticate, identity_function) # /auth
+
+# @jwt.jwt_error_handler
+# def customized_error_handler(error):
+#     return jsonify({
+#         'message': error.description,
+#         'code': error.status_code
+#     }), error.status_code
+
+# config JWT to expire within half an hour
+#app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=1800)
+
+# config JWT auth key name to be 'email' instead of default 'username'
+#app.config['JWT_AUTH_USERNAME_KEY'] = 'email'
+
+api.add_resource(Item, '/item/<string:name>') # http://127.0.0.1:5000/student/Rolf
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(StoreList, '/stores')
+
+api.add_resource(UserRegister, '/register')
+
+if __name__ == '__main__': # this prevents the app from running if we ever import app.py
+    from db import db
+    db.init_app(app)
+    app.run(port=5000, debug=True)
---
+++ models/store.py
@@ -0,0 +1,32 @@
+# this is the internal representatino of what an item is and
+# how it behaves
+
+# import sqlite3
+from db import db
+
+class StoreModel(db.Model):
+    __tablename__ = 'stores'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+
+    #  this is a store back-reference
+    items = db.relationship('ItemModel', lazy='dynamic') # adding lazy='dynamic' means we have to add '.all to our self.items call
+
+    def __init__(self, name):
+        self.name = name
+
+    def json(self):
+        return {'name': self.name, 'items': [item.json() for item in self.items.all()]}
+
+    @classmethod
+    def find_by_name(cls, name):
+        return cls.query.filter_by(name=name).first()
+
+    def save_to_db(self): #insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
